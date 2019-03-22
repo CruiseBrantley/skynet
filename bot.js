0fe8ea348cfe86ff5bffc8ad5d98a002d2cb4a7e
Cruise Brantley <cruise.brantley@gmail.com>
2019-03-22 01:06:19 -0500

    Lots of refactoring

--- bot.js
+++ bot.js
@@ -1,18 +1,22 @@
-var Discord = require("discord.io");
-const publicIp = require("public-ip");
-var logger = require("winston");
-var auth = require("./auth.json");
+const { Command } = require("./Command");
+
+const Discord = require("discord.io");
+const logger = require("winston");
+const auth = require("./auth.json");
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console(), {
 	colorize: true
 });
 logger.level = "debug";
+
 // Initialize Discord Bot
-var bot = new Discord.Client({
+const bot = new Discord.Client({
 	token: auth.token,
 	autorun: true
 });
+exports.bot = bot;
 
 bot.on("ready", function(evt) {
 	logger.info("Connected");
@@ -24,43 +28,25 @@ bot.on("message", function(user, userID, channelID, message, evt) {
 	// Our bot needs to know if it will execute a command
 	// It will listen for messages that will start with `!`
 	if (message.substring(0, 1) == "!") {
-		var args = message.substring(1).split(" ");
-		var cmd = args[0];
+		let args = message.substring(1).split(" ");
+		let cmd = args[0];
 
 		args = args.splice(1);
-		commands(cmd, channelID);
+		command = new Command(user, userID, channelID, cmd, evt);
+		chatCommand(command);
 	}
 });
 
-function commands(cmd, channelID) {
-	switch (cmd.toLowerCase()) {
+function chatCommand(command) {
+	switch (command.cmd.toLowerCase()) {
 		case "ping":
-			bot.sendMessage({
-				to: channelID,
-				message: "Pong!"
-			});
+			command.ping();
 			break;
 		case "server":
-			findIP(channelID);
+			command.serverIP();
 			break;
 		case "help":
-			help();
+			command.help();
 			break;
 	}
-}
-
-async function findIP(channelID) {
-	let ip = await publicIp.v4();
-	bot.sendMessage({
-		to: channelID,
-		message: ip
-	});
-}
-
-function help(channelID) {
-	const message = "Commands are !Ping, !Server, and !Help";
-	bot.sendMessage({
-		to: channelID,
-		message: message
-	});
 }
