7c45b34be0c745966133c87c085356c775a3148d
Cruise Brantley <cruise.brantley@gmail.com>
2019-04-06 02:50:48 -0500

    Attempt to catch message update errors crashing

--- events/botUpdate.js
+++ events/botUpdate.js
@@ -1,17 +1,21 @@
 const { logger } = require("../bot");
 function botUpdate() {
 	return (originalMessage, updatedMessage) => {
-		if (originalMessage.author.bot) return; //ignore bots
-		if (originalMessage != undefined) {
-			logger.info(
-				"User " +
-					originalMessage.author.username +
-					' updated: "' +
-					originalMessage.content +
-					'" to "' +
-					updatedMessage.content +
-					'"'
-			);
+		try {
+			if (originalMessage.author.bot) return; //ignore bots
+			if (originalMessage != undefined) {
+				logger.info(
+					"User " +
+						originalMessage.author.username +
+						' updated: "' +
+						originalMessage.content +
+						'" to "' +
+						updatedMessage.content +
+						'"'
+				);
+			}
+		} catch (err) {
+			console.log("botUpdate error: ", err);
 		}
 	};
 }
