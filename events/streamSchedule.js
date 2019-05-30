339813ebd8b388692c653b03f65ab06276ccf6cd
Cruise Brantley <cruise.brantley@gmail.com>
2019-05-29 21:57:30 -0500

    Updated stream schedule

--- events/streamSchedule.js
+++ events/streamSchedule.js
@@ -4,11 +4,11 @@ const { botAnnounce } = require("./botAnnounce");
 
 streamSchedule = () => {
 	try {
-		schedule.scheduleJob({ hour: 1, minute: 45, dayOfWeek: 3 }, botAnnounce());
-		schedule.scheduleJob({ hour: 1, minute: 45, dayOfWeek: 4 }, botAnnounce());
-		schedule.scheduleJob({ hour: 1, minute: 45, dayOfWeek: 5 }, botAnnounce());
-		schedule.scheduleJob({ hour: 1, minute: 45, dayOfWeek: 6 }, botAnnounce());
-		schedule.scheduleJob({ hour: 1, minute: 45, dayOfWeek: 0 }, botAnnounce());
+		schedule.scheduleJob({ hour: 11, minute: 00, dayOfWeek: 2 }, botAnnounce());
+		schedule.scheduleJob({ hour: 11, minute: 00, dayOfWeek: 3 }, botAnnounce());
+		schedule.scheduleJob({ hour: 11, minute: 00, dayOfWeek: 4 }, botAnnounce());
+		schedule.scheduleJob({ hour: 11, minute: 00, dayOfWeek: 5 }, botAnnounce());
+		schedule.scheduleJob({ hour: 11, minute: 00, dayOfWeek: 6 }, botAnnounce());
 	}
 	catch (err) {
 		logger.info("streamSchedule error:", err);
