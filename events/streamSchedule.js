d2730dd957fc39abd15ef28dc35f1628504fe6b0
Cruise Brantley <cruise.brantley@gmail.com>
2019-05-30 22:15:59 -0500

    Actually fixing the stream schedule

--- events/streamSchedule.js
+++ events/streamSchedule.js
@@ -4,11 +4,11 @@ const { botAnnounce } = require("./botAnnounce");
 
 streamSchedule = () => {
 	try {
-		schedule.scheduleJob({ hour: 11, minute: 00, dayOfWeek: 2 }, botAnnounce());
-		schedule.scheduleJob({ hour: 11, minute: 00, dayOfWeek: 3 }, botAnnounce());
-		schedule.scheduleJob({ hour: 11, minute: 00, dayOfWeek: 4 }, botAnnounce());
-		schedule.scheduleJob({ hour: 11, minute: 00, dayOfWeek: 5 }, botAnnounce());
-		schedule.scheduleJob({ hour: 11, minute: 00, dayOfWeek: 6 }, botAnnounce());
+		schedule.scheduleJob({ hour: 23, minute: 00, dayOfWeek: 2 }, botAnnounce());
+		schedule.scheduleJob({ hour: 23, minute: 00, dayOfWeek: 3 }, botAnnounce());
+		schedule.scheduleJob({ hour: 23, minute: 00, dayOfWeek: 4 }, botAnnounce());
+		schedule.scheduleJob({ hour: 23, minute: 00, dayOfWeek: 5 }, botAnnounce());
+		schedule.scheduleJob({ hour: 23, minute: 00, dayOfWeek: 6 }, botAnnounce());
 	}
 	catch (err) {
 		logger.info("streamSchedule error:", err);
