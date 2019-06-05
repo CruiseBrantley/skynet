eaa292485ab17a4190f0e35c8aa26bfc9223a828
Cruise Brantley <cruise.brantley@gmail.com>
2019-06-05 00:00:05 -0500

    Subscribed to stream updates

--- events/twitch.js
+++ events/twitch.js
@@ -1,4 +1,17 @@
-const {twitchClient} = require("../bot.js");
+const { twitchClient, WebHookListener, bot } = require("../bot.js");
+let listener;
+let subscription;
+WebHookListener.create(twitchClient, { port: 8090 }).then(async (res) => {
+	listener = res;
+	listener.listen();
+	subscription = await listener.subscribeToStreamChanges(process.env.FIRERAVEN_ID, async (stream) => {
+		if (stream) {
+			bot.channels.get("579407168831225869").send(`${stream.userDisplayName} just went live: ${stream.title}`);
+		} else {
+			bot.channels.get("579407168831225869").send(`${stream.userDisplayName} just went offline.`);
+		}
+	});
+});
 
 class Twitch {
 	constructor() { }
