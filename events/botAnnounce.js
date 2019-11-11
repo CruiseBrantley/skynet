4def9d30cf403be5a1cd75f3d71234893f093d1b
Cruise Brantley <cruise.brantley@gmail.com>
2019-11-10 22:36:15 -0600

    Messages now go to correct channel

--- events/botAnnounce.js
+++ events/botAnnounce.js
@@ -2,7 +2,7 @@ const { logger } = require("../bot");
 
 async function botAnnounce(bot, userName, title) {
 	try {
-		bot.channels.get(process.env.TEST_CHANNEL).send(`@everyone ${userName} just went live! ${title} https://www.twitch.tv/${userName}`);
+		bot.channels.get(process.env.ANNOUNCE_CHANNEL).send(`@everyone ${userName} just went live! ${title} https://www.twitch.tv/${userName}`);
 	} catch (err) {
 		logger.info("botAnnounce error: ", err);
 	}
