a46399e571cf9aa3ce62f72fc7f007c39aa53638
Cruise Brantley <cruise.brantley@gmail.com>
2020-02-05 19:49:23 -0600

    Updates to choose between restarting after crash with node botManager, or run normally using node bot

--- bot.js
+++ bot.js
@@ -2,58 +2,65 @@ const dotenv = require("dotenv");
 dotenv.config();
 const Discord = require("discord.js");
 const winston = require("winston");
-// Initialize Discord Bot
-const bot = new Discord.Client();
-const aFunc = async () => {
-	try {
-		await bot.login(process.env.TOKEN);
-		return bot;
-	} catch (err) {
-		console.error("Bot Failed Logging in: ", err)
-		process.exit()
+
+function discordBot() {
+	// Initialize Discord Bot
+	const bot = new Discord.Client();
+	const aFunc = async () => {
+		try {
+			await bot.login(process.env.TOKEN);
+			return bot;
+		} catch (err) {
+			console.error("Bot Failed Logging in: ", err)
+			process.exit()
+		}
 	}
+	aFunc()
+
+	// Configure logger settings
+	const logger = winston.createLogger({
+		level: "info",
+		format: winston.format.json(),
+		defaultMeta: { service: "user-service" },
+		transports: [new winston.transports.File({ filename: "./logs/combined.log" })]
+	});
+	//debug logging under here, remove for prod
+	logger.add(
+		new winston.transports.Console({
+			format: winston.format.simple()
+		})
+	);
+	module.exports.logger = logger;
+
+	bot.on("ready", () => {
+		logger.info("Connected");
+		logger.info("Logged in as: ");
+		logger.info(bot.user.username + " - (" + bot.user.id + ")");
+		bot.user.setActivity(process.env.ACTIVITY);
+	});
+
+	bot.on("error", err => {
+		logger.info("Encountered an error: ", err);
+	});
+
+	const { botUpdate } = require("./events/botUpdate");
+	const { botMessage } = require("./events/botMessage");
+	const { botDelete } = require("./events/botDelete");
+
+	const { server } = require("./server/server")
+	server(bot)
+
+	// twitterChannelInit();
+
+	bot.on("message", botMessage(bot));
+
+	bot.on("messageUpdate", botUpdate());
+
+	bot.on("messageDelete", botDelete());
+
+	module.exports.bot = bot;
 }
-aFunc()
 
-// Configure logger settings
-const logger = winston.createLogger({
-	level: "info",
-	format: winston.format.json(),
-	defaultMeta: { service: "user-service" },
-	transports: [new winston.transports.File({ filename: "./logs/combined.log" })]
-});
-//debug logging under here, remove for prod
-logger.add(
-	new winston.transports.Console({
-		format: winston.format.simple()
-	})
-);
-module.exports.logger = logger;
+discordBot()
 
-bot.on("ready", () => {
-	logger.info("Connected");
-	logger.info("Logged in as: ");
-	logger.info(bot.user.username + " - (" + bot.user.id + ")");
-	bot.user.setActivity(process.env.ACTIVITY);
-});
-
-bot.on("error", err => {
-	logger.info("Encountered an error: ", err);
-});
-
-const { botUpdate } = require("./events/botUpdate");
-const { botMessage } = require("./events/botMessage");
-const { botDelete } = require("./events/botDelete");
-
-const { server } = require("./server/server")
-server(bot)
-
-// twitterChannelInit();
-
-bot.on("message", botMessage(bot));
-
-bot.on("messageUpdate", botUpdate());
-
-bot.on("messageDelete", botDelete());
-
-module.exports.bot = bot
+module.exports.discordBot = discordBot;
---
+++ botManager.js
@@ -0,0 +1,14 @@
+const { discordBot } = require('./bot')
+const cluster = require('cluster');
+
+if (cluster.isMaster) {
+	cluster.fork();
+
+	cluster.on('exit', function (worker, code, signal) {
+		cluster.fork();
+	});
+}
+
+if (cluster.isWorker) {
+	discordBot()
+}
