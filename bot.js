58c00c3a41f00fa9d05d3f6fce33dd63846c75fa
Cruise Brantley <cruise.brantley@gmail.com>
2020-05-10 18:58:23 -0500

    No reason to rely on env for activity

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ function discordBot() {
 		logger.info("Connected");
 		logger.info("Logged in as: ");
 		logger.info(bot.user.username + " - (" + bot.user.id + ")");
-		bot.user.setActivity(process.env.ACTIVITY, { type: 'LISTENING' });
+		bot.user.setActivity('for John Connor', { type: 'LISTENING' });
 	});
 
 	bot.on("error", err => {
