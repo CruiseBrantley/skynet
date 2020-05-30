d4571f35cd518c040f9a16e02e758ce6e2df1d63
Cruise Brantley <cruise.brantley@gmail.com>
2020-05-30 04:31:07 -0500

    oauthToken is used for game request as well, refreshed at same time as resubscribes

--- server/server.js
+++ server/server.js
@@ -6,36 +6,51 @@ const logger = require('../logger')
 const oauth = require('./oauth')
 const express = require('express')
 const server = express()
-const port = process.env.NODE_ENV === 'dev' ? 3001 : process.env.TWITCH_LISTEN_PORT
+const port =
+  process.env.NODE_ENV === 'dev' ? 3001 : process.env.TWITCH_LISTEN_PORT
 console.log(port)
 
 server.use(bodyParser.json())
 
 let streamID
-
-async function subscribe (id, oauthToken) {
+let oauthToken
+async function subscribe (id) {
   const data = {
     'hub.callback': `http://${await publicIp.v4()}:${port}/`,
     'hub.mode': 'subscribe',
     'hub.lease_seconds': 86400,
     'hub.topic': `https://api.twitch.tv/helix/streams?user_id=${id}`
   }
-  axios.post('https://api.twitch.tv/helix/webhooks/hub', data, { headers: { 'Client-ID': process.env.TWITCH_CLIENTID, Authorization: `Bearer ${oauthToken}` } })
-    .then(res => logger.info('Successfully subscribed to Twitch Updates for ' + id))
+  axios
+    .post('https://api.twitch.tv/helix/webhooks/hub', data, {
+      headers: {
+        'Client-ID': process.env.TWITCH_CLIENTID,
+        Authorization: `Bearer ${oauthToken}`
+      }
+    })
+    .then(res =>
+      logger.info('Successfully subscribed to Twitch Updates for ' + id)
+    )
     .catch(err => logger.info('Failed Subscribing for' + id + err))
 }
 
 async function subscribeAll () {
-  const oauthToken = await oauth()
-  subscribe(process.env.FIRERAVEN_ID, oauthToken)
-  subscribe(process.env.CYPHANE_ID, oauthToken)
-  subscribe(process.env.CHA_ID, oauthToken)
-  subscribe(process.env.SIRI4N_ID, oauthToken)
-  subscribe(process.env.BFD_ID, oauthToken)
+  oauthToken = await oauth()
+  subscribe(process.env.FIRERAVEN_ID)
+  subscribe(process.env.CYPHANE_ID)
+  subscribe(process.env.CHA_ID)
+  subscribe(process.env.SIRI4N_ID)
+  subscribe(process.env.BFD_ID)
 }
 
-async function getGameInfo (id) {
-  return axios.get(`https://api.twitch.tv/helix/games?id=${id}`, { headers: { 'Client-ID': process.env.TWITCH_CLIENTID } })
+function getGameInfo (id) {
+  return axios
+    .get(`https://api.twitch.tv/helix/games?id=${id}`, {
+      headers: {
+        'Client-ID': process.env.TWITCH_CLIENTID,
+        Authorization: `Bearer ${oauthToken}`
+      }
+    })
     .then(res => {
       if (res && res.data && res.data.data && res.data.data.length) {
         const response = res.data.data[0]
@@ -57,24 +72,37 @@ function setupServer (bot) {
   server.get('/', async (req, res) => {
     // Called on initial subscription
     logger.info('Get: ' + req.query['hub.challenge'])
-    res.status(200).type('text/plain').send(req.query['hub.challenge'])
+    res
+      .status(200)
+      .type('text/plain')
+      .send(req.query['hub.challenge'])
   })
 
   server.post('/', async (req, res) => {
     // Called when new stream is detected
     // streamID is kept to ensure there aren't duplicate updates
     logger.info('Post Received.')
-    if (req.body && req.body.data && req.body.data.length > 0 && req.body.data[0].id !== streamID) {
+    if (
+      req.body &&
+      req.body.data &&
+      req.body.data.length > 0 &&
+      req.body.data[0].id !== streamID
+    ) {
       const response = req.body.data[0]
       const gameInfo = await getGameInfo(response.game_id)
       const betterResponse = { ...response, ...gameInfo }
       botAnnounce(bot, betterResponse)
       streamID = betterResponse.id
     }
-    res.status(200).type('text/plain').send(req.query['hub.challenge'])
+    res
+      .status(200)
+      .type('text/plain')
+      .send(req.query['hub.challenge'])
   })
 
-  server.listen(port, () => logger.info(`Twitch updates listening on port: ${port}!`))
+  server.listen(port, () =>
+    logger.info(`Twitch updates listening on port: ${port}!`)
+  )
   return server
 }
 
