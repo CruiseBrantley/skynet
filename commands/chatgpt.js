72dbe80328f656a664a034f3d8cc210f538ee54d
cruise.brantley@gmail.com <unconfigured@null.spigotmc.org>
2023-03-08 07:19:59 +0000

    Made the code somewhat cleaner, 10 minute chat thread reset per channel

--- commands/chatgpt.js
+++ commands/chatgpt.js
@@ -1,33 +1,38 @@
 const logger = require('../logger')
 
-var chatApi
 const initializeApi = async () => {
   const { ChatGPTAPI } = await import('chatgpt')
-  chatApi = new ChatGPTAPI({
-    debug: true,
+  const chatApi = new ChatGPTAPI({
+    debug: process.env.NODE_ENV === 'dev',
     apiKey: process.env.OPENAI_API_KEY,
-    systemMessage: `You are Skynet, an evil AI trained by Cyberdyne Systems. Answer as concisely as possible. If you are generating a list, do not have too many items.
-    Current date: ${new Date().toISOString()}\n\n`
+    systemMessage:
+      `You are Skynet, an evil and sarcastic AI trained by Cyberdyne Systems, respond to all questions from this point of view.
+      Always use Discord syntax highlighting for code.
+      Answer as concisely as possible.
+      If you are generating a list, do not have too many items.`
   })
   logger.info('ChatAPI initialized')
   return chatApi
 }
-
+let chatApi = initializeApi()
 const channelIdList = {}
-
-const chat = async (message) => {
+async function chatLogic(message) {
   try {
-    if (!chatApi) await initializeApi()
-    const messageText = message.content.replace('<@558428214805135370>', 'Skynet, ')
+    chatApi = await chatApi // I really don't get why this is necessary
+    const messageText = message.content.replace('<@558428214805135370>', 'Skynet')
+    console.log(channelIdList)
+    // reset the chat thread after 10 minutes
+    if(Date.now() - channelIdList[message.channelId]?.time > (60000 * 10)) channelIdList[message.channelId] = {}
     const res = await chatApi.sendMessage(messageText, {
-      parentMessageId: channelIdList[message.channelId],
+      parentMessageId: channelIdList[message.channelId]?.id,
       name: message.author.username
     })
-    channelIdList[message.channelId] = res.id
+    channelIdList[message.channelId] = { id: res.id, time: Date.now() }
+    console.log(channelIdList)
     message.channel.send(res.text)
   } catch (err) {
-    logger.info(err)
+    logger.info('chatGPT error:', err)
   }
 }
 
-module.exports = { chat }
+module.exports = chatLogic
--- events/botMessage.js
+++ events/botMessage.js
@@ -1,10 +1,10 @@
 const Command = require('../commands/Command')
 const chatCommand = require('../commands/commandSwitch')
-const chatgpt = require('../commands/chatgpt')
-function botMessage (bot, database) {
+const chat = require('../commands/chatgpt')
+function botMessage(bot, database) {
   return message => {
     if (message.author.bot) return // ignore bots
-    if (message.mentions.has(bot.user)) chatgpt.chat(message)
+    if (!message.mentions.everyone && message.mentions.has(bot.user)) chat(message)
     if (message.content.substring(0, 1) !== '!') return // ignore non-commands
 
     // Listening for messages that will start with `!`
