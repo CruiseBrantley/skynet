1198284d06728281a5222d9eabc386c45848a48f
cruise.brantley@gmail.com <unconfigured@null.spigotmc.org>
2024-01-11 08:09:34 +0000

    ngrok fixes

--- server/ngrok.js
+++ server/ngrok.js
@@ -1,17 +1,20 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const logger = require('../logger')
 const ngrok = require('ngrok')
 const port = process.env.TWITCH_LISTEN_PORT
 const authtoken = process.env.NGROK_AUTH_TOKEN
+const subdomain = process.env.NGROK_SUBDOMAIN
 
 const getURL = async () => {
     try {
-            // await ngrok.disconnect()
             const url = await ngrok.connect({
             addr: port,
             authtoken
+            // subdomain
         })
-        logger.info('Ngrok URL: ' + url)
-        return url
+            logger.info('Ngrok URL: ' + url)
+            return url
     } catch (err) {
         console.error(err)
         logger.error('Ngrok Error: ' + err)
